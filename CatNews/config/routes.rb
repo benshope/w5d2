776242fbf1c0ee8acd4f2ec4cd48170fecb60206
Dylan Patrick Clark <dylanclark112@gmail.com>
2013-04-09 19:09:15 -0700

    Update routes.rb

--- CatNews/config/routes.rb
+++ CatNews/config/routes.rb
@@ -1,13 +1,13 @@
 CatNews::Application.routes.draw do
   root :to => "posts#index"
 
-  get '/signin' => 'sessions#new'
+  get '/signin' => 'sessions#new' #REV: good, dont want to do this with a match.
   post '/signin' => 'sessions#create'
   match '/signout' => 'sessions#destroy'
 
 
   resources :comments, :except => [:edit, :update]
-  resources :posts, :except => [:edit, :update]
+  resources :posts, :except => [:edit, :update] #REV: perhaps it makes sense to nest comments under posts?
   resources :users
   resources :emails, :except => [:edit, :update]
   #post '/user/add_karma'
